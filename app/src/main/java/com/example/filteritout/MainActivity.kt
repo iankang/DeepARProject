bfebfbacdfbd50f69ea3d035ffa5acea2e257719
Ian Kang'ethe <ian.kangethe@dtone.com>
2020-01-26 23:27:36 +0300

    initializing the deepAR libraries

--- app/src/main/java/com/example/filteritout/MainActivity.kt
+++ app/src/main/java/com/example/filteritout/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.filteritout
 
+import ai.deepar.ar.AREventListener
+import ai.deepar.ar.CameraResolutionPreset
 import ai.deepar.ar.DeepAR
 import android.Manifest
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.hardware.Camera
 import android.os.Bundle
-import android.widget.ImageButton
+import android.util.DisplayMetrics
+import android.view.Surface
+import android.view.SurfaceHolder
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -12,11 +19,16 @@ import androidx.databinding.DataBindingUtil
 import com.example.filteritout.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),SurfaceHolder.Callback,AREventListener {
 
     lateinit var deepAR:DeepAR
     private lateinit var binding:ActivityMainBinding
     private lateinit var filterMutableList:MutableList<String?>
+    private val defaultCameraDevice:Int = Camera.CameraInfo.CAMERA_FACING_FRONT
+    private var cameraDevice:Int = defaultCameraDevice
+    private lateinit var cameraGrabber:CameraGrabber
+
+    var screensOrientation: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +47,7 @@ class MainActivity : AppCompatActivity() {
         }
         else{
 
-            //initialize hapa
+            initialize()
 
         }
     }
@@ -50,7 +62,7 @@ class MainActivity : AppCompatActivity() {
                 if(grantResult != PackageManager.PERMISSION_GRANTED){
                     return
                 }
-                //initialize hapa
+                initialize()
             }
         }
     }
@@ -65,10 +77,32 @@ class MainActivity : AppCompatActivity() {
     private fun initializeViews() {
 
         binding.apply {
-            previousMask.setOnClickListener(goToPrevious())
+            previousMask.setOnClickListener{goToPrevious()}
             nextMask.setOnClickListener { goToNext() }
-            surface.setOnClickListener(deepAR.onClick())
+            surface.setOnClickListener{deepAR.onClick()}
+            surface.holder.addCallback(this@MainActivity)
+            recordButton.setOnClickListener{deepAR.takeScreenshot()}
+            switchCamera.setOnClickListener{
+                if(cameraGrabber.currCameraDevice == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                    cameraDevice = Camera.CameraInfo.CAMERA_FACING_BACK
+                }
+                else{
+                    cameraDevice = Camera.CameraInfo.CAMERA_FACING_FRONT
+                }
+                cameraGrabber.changeCameraDevice(cameraDevice)
+            }
+
         }
+
+
+
+    }
+
+    private fun goToNext() {
+
+    }
+
+    private fun goToPrevious() {
 
     }
 
@@ -94,7 +128,108 @@ class MainActivity : AppCompatActivity() {
         filterMutableList.add("grumpycat")    }
 
     private fun initializeDeepAR() {
+        deepAR = DeepAR(this)
+        deepAR.setLicenseKey(getString(R.string.license_key))
+        deepAR.initialize(this,this)
+
+        cameraGrabber = CameraGrabber(cameraDevice)
+        screensOrientation = getScreenOrientation()
+
+        when(screensOrientation){
+            ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE -> cameraGrabber.screenOrientation = 90
+            ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE -> cameraGrabber.screenOrientation = 270
+            else -> cameraGrabber.screenOrientation = 0
+        }
+
+        cameraGrabber.resolutionPreset = CameraResolutionPreset.P640x480
+
+    }
+
+    private fun getScreenOrientation(): Int {
+        val rotation = windowManager.defaultDisplay.rotation
+        val dm = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(dm)
+        val width = dm.widthPixels
+        val height = dm.heightPixels
+        val orientation: Int
+        // if the device's natural orientation is portrait:
+        orientation = if ((rotation == Surface.ROTATION_0
+                    || rotation == Surface.ROTATION_180) && height > width ||
+            (rotation == Surface.ROTATION_90
+                    || rotation == Surface.ROTATION_270) && width > height
+        ) {
+            when (rotation) {
+                Surface.ROTATION_0 -> ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                Surface.ROTATION_90 -> ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                Surface.ROTATION_180 -> ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT
+                Surface.ROTATION_270 -> ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE
+                else -> ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            }
+        } else {
+            when (rotation) {
+                Surface.ROTATION_0 -> ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                Surface.ROTATION_90 -> ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                Surface.ROTATION_180 -> ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE
+                Surface.ROTATION_270 -> ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT
+                else -> ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            }
+        }
+        return orientation
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun shutdownFinished() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun faceVisibilityChanged(p0: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun videoRecordingFailed() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun videoRecordingPrepared() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initialized() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun error(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun screenshotTaken(p0: Bitmap?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun effectSwitched(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun videoRecordingStarted() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun videoRecordingFinished() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun imageVisibilityChanged(p0: String?, p1: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
